c7cff4ddc17b9fb81effc7e41e90cbcb3f6ed815
changye <changye17@163.com>
2015-01-23 20:41:29 +0800

    Start modify  LinkController

--- src/EnbabyDBManagerBundle/Controller/LinksController.php
+++ src/EnbabyDBManagerBundle/Controller/LinksController.php
@@ -12,49 +12,25 @@ use EnbabyDBManagerBundle\Entity\Books;
 
 class LinksController extends Controller
 {
-    public function getBooksInSeries($seriesId)
+    public function unlinkAction($seriesId)
     {
-	$em = $this->getDoctrine()->getManager();
-	$query = $em->createQuery('SELECT book.isbn,book.displayName FROM EnbabyDBManagerBundle:Books book,EnbabyDBManagerBundle:Links link WHERE link.series = :seriesid AND book.isbn = link.isbn')->setParameter('seriesid', $seriesId);
-	$books = $query->getResult();
-	return $books;
+        $em = $this->getDoctrine()->getManager();
+        $series = $em->getRepository('EnbabyDBManagerBundle:Series')->findOneById($seriesId);
+        $books = array();
+
+        if(!$series)
+        {
+                $series = new Series();
+                $series->setId($this->getNextSeriesId());
+        }else{
+                $lc = $this->get('EnbabyDBManagerBundle.Services.LinksDB');
+                $books = $lc->getBooksInSeries($seriesId);
+        }
+
+
+        return $this->render('EnbabyDBManagerBundle:Series:series.html.twig',array('series'=>$series,'books'=>$books));
     }
 
-    public function removeBookFromSeries($isbn,$seriesId)
-    {
-	$em = $this->getDoctrine()->getManager();
-	$link = $em->getRepository('EnbabyDBManagerBundle:Links')->findOneBy(array('isbn'=>$isbn,'series'=>$seriesId));
-	if($link)
-	{
-		$em->delete($link);
-		return 1;
-	}else{
-		return 0;
-	}
-    }
-
-    public function addBookToSeries($isbn,$seriesId)
-    {
-	$em = $this->getDoctrine()->getManager();
-        $link = $em->getRepository('EnbabyDBManagerBundle:Links')->findOneBy(array('isbn'=>$isbn,'series'=>$seriesId));
-	if(!$link)
-	{
-		$link = new Links();
-		$link->setSeries($seriesId);
-		$link->setIsbn($isbn);
-		$em->persist($link);
-		$em->flush();
-	}	
-        return 1;
-    }
-
-    public function getSeriesOfABook($isbn)
-    {
-	$em = $this->getDoctrine()->getManager();
-        $query = $em->createQuery('SELECT series.id FROM EnbabyDBManagerBundle:Series series,EnbabyDBManagerBundle:Links link WHERE link.isbn = :isbn AND series.id = link.series')->setParameter('isbn', $isbn);
-        $series = $query->getResult();
-        return $series;
-    }
 
 
 }
