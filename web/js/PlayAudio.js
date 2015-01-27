6573523436ba006876b856bb1a02143ef81f0c19
changye-vaio <changye17@163.com>
2015-01-27 16:01:31 +0800

    fix bugs

--- web/js/PlayAudio.js
+++ web/js/PlayAudio.js
@@ -19,7 +19,7 @@ $(document).on("pageinit","#mobilePage",function( event ){
  // 	});
 
 
- 	audioSrc = document.audioFiles.split(";");
+ 	var audioSrc = document.audioFiles.split(";");
  	for (var i = audioSrc.length - 1; i >= 0; i--) {
  		var source = $("<source></source>");
  		source.attr('src',audioSrc[i]);
@@ -35,16 +35,15 @@ $(document).on("pageinit","#mobilePage",function( event ){
  		}else{
  			song.pause();
  		}
- 		
  	});
 
 
- 	$("audio").bind("loadstart",function () {
- 		theme = $.mobile.loader.prototype.options.theme;
- 		$.mobile.loading( "show", {text: "音频载入中...",textVisible: true , theme: "b" , textonly: true, html: ""});
- 	});
- 	$("audio").bind("canplay",function () {
- 		$.mobile.loading("hide");
- 	});
+ 	// $("audio").bind("loadstart",function () {
+ 	// 	theme = $.mobile.loader.prototype.options.theme;
+ 	// 	$.mobile.loading( "show", {text: "音频载入中...",textVisible: true , theme: "b" , textonly: true, html: ""});
+ 	// });
+ 	// $("audio").bind("canplay",function () {
+ 	// 	$.mobile.loading("hide");
+ 	// });
 
 });
